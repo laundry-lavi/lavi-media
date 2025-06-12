acbbfc8170232f8938e818341ded2f6d038660f5
https-dre <diaso.andre@outlook.com>
2025-06-12 17:08:55 -0300

    remove unused import

--- src/router.ts
+++ src/router.ts
@@ -2,14 +2,13 @@ import { FastifyInstance } from "fastify";
 import { PublicObjectController } from "./controllers/public-object.controller";
 import { addPublicObject, getPublicObjects } from "./schemas/public-objects";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { z } from "zod";
 
 export const router = async (server: FastifyInstance) => {
 	const app = server.withTypeProvider<ZodTypeProvider>();
 	const publicObjectController = new PublicObjectController();
 
 	app.put("/objects/public", { schema: addPublicObject }, publicObjectController.addObject);
-	
+
 	app.get(
 		"/objects/public/:object_id",
 		{ schema: getPublicObjects },
