5e4b032a1db651b7691ab3ade5091ef08cfa28d0
https-dre <diaso.andre@outlook.com>
2025-06-12 15:47:54 -0300

    feat: add "/objects/public/:id"

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,18 @@
 import { FastifyInstance } from "fastify";
-import { PublicObjectController } from "./controllers/public-object.controller"
-import { addPublicObject } from "./schemas/public-objects";
+import { PublicObjectController } from "./controllers/public-object.controller";
+import { addPublicObject, getPublicObjects } from "./schemas/public-objects";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
 
 export const router = async (server: FastifyInstance) => {
 	const app = server.withTypeProvider<ZodTypeProvider>();
 	const publicObjectController = new PublicObjectController();
 
 	app.put("/objects/public", { schema: addPublicObject }, publicObjectController.addObject);
+	
+	app.get(
+		"/objects/public/:object_id",
+		{ schema: getPublicObjects },
+		publicObjectController.getObject
+	);
 };
