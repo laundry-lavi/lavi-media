59cba77621270433e28f1e6485cbabee694db803
https-dre <diaso.andre@outlook.com>
2025-07-19 21:48:34 -0300

    remove unused database vars

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,23 @@
 const EnvConfig = new Map<string, string>(
-	Object.entries(process.env).filter(
-		(entry): entry is [string, string] => entry[1] !== undefined
-	)
+  Object.entries(process.env).filter(
+    (entry): entry is [string, string] => entry[1] !== undefined
+  )
 );
 
 const vars = [
-	"DB_HOST",
-	"DB_PORT",
-	"DB_USER",
-	"DB_PASSWORD",
-	"DB_NAME",
-	"AWS_ACCESS_KEY_ID",
-	"AWS_SECRET_ACCESS_KEY",
-	"AWS_REGION",
+  //"DB_HOST",
+  //"DB_PORT",
+  //"DB_USER",
+  //"DB_PASSWORD",
+  //"DB_NAME",
+  "AWS_ACCESS_KEY_ID",
+  "AWS_SECRET_ACCESS_KEY",
+  "AWS_REGION",
 ];
 
 const missingVars = vars.filter((v) => !EnvConfig.has(v));
 if (missingVars.length) {
-	throw new Error(`Missing env vars: ${missingVars.join(", ")}`);
+  throw new Error(`Missing env vars: ${missingVars.join(", ")}`);
 }
 
 export default EnvConfig;
