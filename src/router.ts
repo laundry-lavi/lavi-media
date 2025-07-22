5cbbd8f419dd7dc34781117e70c60cf67dd81fab
https-dre <diaso.andre@outlook.com>
2025-07-21 22:47:39 -0300

    feat: add delete route

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,31 @@
 import { FastifyInstance } from "fastify";
 import { PublicObjectController } from "./controllers/public-object.controller";
-import { addPublicObject, getPublicObjects } from "./schemas/public-objects";
+import {
+  addPublicObject,
+  deletePublicObject,
+  getPublicObjects,
+} from "./schemas/public-objects";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 
 export const router = async (server: FastifyInstance) => {
-	const app = server.withTypeProvider<ZodTypeProvider>();
-	const publicObjectController = new PublicObjectController();
+  const app = server.withTypeProvider<ZodTypeProvider>();
+  const publicObjectController = new PublicObjectController();
 
-	app.put("/objects/public", { schema: addPublicObject }, publicObjectController.addObject);
+  app.put(
+    "/objects/public",
+    { schema: addPublicObject },
+    publicObjectController.addObject
+  );
 
-	app.get(
-		"/objects/public/:object_id",
-		{ schema: getPublicObjects },
-		publicObjectController.getObject
-	);
+  app.get(
+    "/objects/public/:object_id",
+    { schema: getPublicObjects },
+    publicObjectController.getObject
+  );
+
+  app.delete(
+    "/objects/public/:object_id",
+    { schema: deletePublicObject },
+    publicObjectController.deleteObject
+  );
 };
--- src/schemas/public-objects.ts
+++ src/schemas/public-objects.ts
@@ -1,40 +1,52 @@
 import { z } from "zod";
 
 export const addPublicObject = {
-	description: "add-object",
-	summary: "Upload one public object",
-	tags: ["public"],
-	response: {
-		201: z.object({
-			message: z.string(),
-			details: z.object({
-				key: z.string(),
-				lastModified: z.date(),
-				size: z.number(),
-				url: z.string(),
-			}),
-		}),
-	},
+  description: "add-object",
+  summary: "Upload one public object",
+  tags: ["public"],
+  response: {
+    201: z.object({
+      message: z.string(),
+      details: z.object({
+        key: z.string(),
+        lastModified: z.date(),
+        size: z.number(),
+        url: z.string(),
+      }),
+    }),
+  },
 };
 
 export const getPublicObjects = {
-	params: z.object({
-		object_id: z.string(),
-	}),
-	response: {
-		201: z.object({
-			data: z.object({
-				objects: z.array(
-					z.object({
-						key: z.string(),
-						url: z.string(),
-						size: z.number(),
-						lastModified: z.date(),
-					})
-				),
-			}),
-		}),
-	},
-	tags: ["public"],
-	summary: "Get an objects list by your key prefix or your complete key",
+  params: z.object({
+    object_id: z.string(),
+  }),
+  response: {
+    201: z.object({
+      data: z.object({
+        objects: z.array(
+          z.object({
+            key: z.string(),
+            url: z.string(),
+            size: z.number(),
+            lastModified: z.date(),
+          })
+        ),
+      }),
+    }),
+  },
+  tags: ["public"],
+  summary: "Get an objects list by your key prefix or your complete key",
+};
+
+export const deletePublicObject = {
+  params: z.object({
+    object_id: z.string(),
+  }),
+
+  response: {
+    204: z.null(),
+  },
+  tags: ["public"],
+  summary: "Delete an public object by your complete key",
 };
