38a868e6270c1ac76872b7aed3f490bb5a579f87
https-dre <diaso.andre@outlook.com>
2025-08-16 16:32:53 -0300

    feat: implement 'buildApp' and new custom logger

--- src/server.ts
+++ src/server.ts
@@ -1,52 +1,15 @@
-import fastify from "fastify";
-import {
-  validatorCompiler,
-  serializerCompiler,
-  jsonSchemaTransform,
-} from "fastify-type-provider-zod";
-
-import fastifyMultipart from "@fastify/multipart";
-import fastifySwagger from "@fastify/swagger";
-import fastifySwaggerUi from "@fastify/swagger-ui";
-import { router } from "./router";
-import { ServerErrorHandler } from "./error-handler";
-
-const app = fastify({
-  ignoreTrailingSlash: true,
-});
+import { buildApp } from "./buildApp";
+import { logger } from "./logger";
 
 const run = async () => {
-  await app.register(fastifyMultipart);
-
-  await app.register(fastifySwagger, {
-    swagger: {
-      consumes: ["application/json", "multipart/form-data"],
-      produces: ["application/json"],
-      info: {
-        title: "Lavi Media API",
-        description: "An API based on AWS S3 for storing files",
-        version: "1.0.0",
-      },
-    },
-    transform: jsonSchemaTransform,
-  });
-
-  await app.register(fastifySwaggerUi, {
-    routePrefix: "/docs",
-  });
-
-  app.setValidatorCompiler(validatorCompiler);
-  app.setSerializerCompiler(serializerCompiler);
-
-  app.setErrorHandler(ServerErrorHandler);
-
-  await router(app);
+  const app = buildApp();
 
   const port = process.env.PORT || 7700;
 
   try {
     const address = await app.listen({ port: Number(port), host: "0.0.0.0" });
-    console.log("Server running: ", address);
+    logger.info(`Application port = ${port}`);
+    logger.info("Server running: " + address);
   } catch (err) {
     console.error(err);
     process.exit(1);
